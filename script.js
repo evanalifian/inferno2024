cf4eba0a7b3a47bd9aa2527e3aa50fb93c228385
yohanesokta <yohanesoktayt72@gmail.com>
2024-09-13 21:47:48 +0700

    update

--- script.js
+++ script.js
@@ -10,15 +10,33 @@ async function friendListsElement() {
   const lists = `
     ${data.map(i => `
       <li>
-        <figure class="card" title="${i.nama}">
-          <div class="card_image">
-            <img src="${i.fotoselfie}" alt="${i.nama}" width="100%">
-          </div>
-          <figcaption class="card_description">
-            <span>${i.nama}</span>
-            <span>${parseInt(i.nim)}</span>
-          </figcaption>
-        </figure>
+        ${i.web ? `
+          <a href="${i.web}" target="_blank">
+            <figure class="card" title="${i.nama}">
+              <div class="card_image">
+                <img src="${i.fotoselfie}" alt="${i.nama}" width="100%">
+              </div>
+              <span class="card_arrow_link">
+                <span>Visit site</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-up-right"><path d="M13 5H19V11"/><path d="M19 5L5 19"/></svg>
+              </span>
+              <figcaption class="card_description">
+                <span>${i.nama}</span>
+                <span>${parseInt(i.nim)}</span>
+              </figcaption>
+            </figure>
+          </a>
+        ` : `
+          <figure class="card" title="${i.nama}">
+            <div class="card_image">
+              <img src="${i.fotoselfie}" alt="${i.nama}" width="100%">
+            </div>
+            <figcaption class="card_description">
+              <span>${i.nama}</span>
+              <span>${parseInt(i.nim)}</span>
+            </figcaption>
+          </figure>
+        `}
       </li>
     `).join("")}
   `;
